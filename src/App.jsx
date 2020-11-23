0d22f4b6f571080cc7f113a4f7c778d3fc814eaa
jiangmin <jiangmin@hand-china.com>
2020-11-23 10:48:46 +0800

    state 异步变同步

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,39 @@
 import React from 'react';
+import { DatePicker, Button } from 'hzero-ui';
+import 'hzero-ui/dist/hzero-ui.css';
+import RegistrationForm from './RegistrationForm';
+
 import SetStateDemo from './setStateDemo';
 // import MyNav from './MyNav';
 // import StateComponent from './StateComponent';
 // import ComponentLife from './ComponentLife';
 
+const { RangePicker } = DatePicker;
+
 export default class App extends React.Component {
 
-    constructor(){
+    constructor() {
         super();
-        this.state ={
+        this.state = {
             title: "文本1"
         }
 
     }
 
-    updateTitle = () =>{
+    updateTitle = () => {
         let title = this.state.title;
         this.setState({
-            title: title+="a"
+            title: title += "a"
         })
+    }
+
+    onChange(value, dateString) {
+        console.log('Selected Time: ', value);
+        console.log('Formatted Selected Time: ', dateString);
+    }
+
+    onOk(value) {
+        console.log('onOk: ', value);
     }
 
     render() {
@@ -34,8 +49,36 @@ export default class App extends React.Component {
                 {/* <ComponentLife title = {this.state.title} /> */}
                 {/* <button onClick = {this.updateTitle}>修改title</button> */}
                 <SetStateDemo />
+                <DatePicker />
+                <Button type="primary">Primary</Button>
+                <Button>Default</Button>
+                <Button type="dashed">Dashed</Button>
+                <Button type="danger">Danger</Button>
+                <div>
+                    <div>
+                        <DatePicker
+                            showTime
+                            format="YYYY-MM-DD HH:mm:ss"
+                            placeholder="Select Time"
+                            onChange={this.onChange}
+                            onOk={this.onOk}
+                        />
+                        <br />
+                        <RangePicker
+                            showTime={{ format: 'HH:mm' }}
+                            format="YYYY-MM-DD HH:mm"
+                            placeholder={['Start Time', 'End Time']}
+                            onChange={this.onChange}
+                            onOk={this.onOk}
+                        />
+                    </div>
+                </div>
+                <div >
+                    <RegistrationForm />
+                </div>
             </div>
-           
+
+
         )
     }
 }
--- src/setStateDemo.jsx
+++ src/setStateDemo.jsx
@@ -9,27 +9,29 @@ export default class SetStateDemo extends React.Component {
         }
     }
 
-    inscrement = () => {
-        let count = this.state.count;
-        this.setState({
-            count: count + 1
-        },
-            //同步获取state中的count
-            () => {
-                console.log("同步"+this.state.count);
-            }
-        );
-        //异步获取state中的count
-        console.log("异步",this.state.count);
+    async inscrement() {
+        // let count = this.state.count;
+        // this.setState({
+        //     count: count + 1
+        // },
+        //     //同步获取state中的count
+        //     () => {
+        //         console.log("同步"+this.state.count);
+        //     }
+        // );
+        // //异步获取state中的count
+        // console.log("异步",this.state.count);
+        await this.setState({count: this.state.count+1});
+        console.log("同步：", this.state.count);
     }
+ 
 
     render() {
         return (
             <div>
                 setState 同步还是异步
                 <p>{this.state.count}</p>
-                <button onClick={this.inscrement}>修改</button>
-
+                <button onClick={this.inscrement.bind(this)}>修改</button>
             </div>
         )
     }
